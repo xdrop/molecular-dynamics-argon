2b5fcc7c6e1978402116b348dbeb826d6b6b1792
xdrop <pastosdev@gmail.com>
2016-11-15 13:37:12 +0000

    Vertlet + Cell list works

--- argonsim/argonsim/main.cpp
+++ argonsim/argonsim/main.cpp
@@ -1,15 +1,371 @@
+// Translate this file with
 //
-//  main.cpp
-//  argonsim
+// g++ -O3 spaceboddies.c -o spaceboddies
 //
-//  Created by xdrop on 03/11/2016.
-//  Copyright © 2016 xdrop. All rights reserved.
+// Run it with
 //
+// ./spaceboddies
+//
+// Open Paraview (www.paraview.org) and do the following:
+// - Select File/open and select all the results files. Press the Apply button.
+// - Click into the left visualisation window (usually titled Layout #1).
+// - Click the result-* item in the window Pipeline Browser. Ensure that your Layout #1 and the item result-* is marked.
+// - Select Filters/Alphabetical/TableToPoints. Press Apply button.
+// - Switch the representation (on top) from Surface into Points.
+// - Press the play button and adopt colours and point sizes.
+// - For some Paraview versions, you have to mark your TableToPoints item (usually called TableToPoints1) and explicitly select that X Column is x, Y Column is y, and Z Column is z.
+// - What is pretty cool is the Filter TemporalParticlesToPathlines. If you set Mask Points to 1, you see a part of the trajactory.
+//
+// (C) 2015 Tobias Weinzierl
 
+#include <fstream>
+#include <sstream>
+#include <cmath>
 #include <iostream>
+#include <cassert>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+const int N = 100;
+double pos[N][7];
+
+#define EMPTY -1
+
+double v[N][3];
+
+const double a = 0.01;
+
+const double s = a;
+const double cutoff = 10 * s;
+const double rSqrd = cutoff * cutoff;
+const double cellSize = 0.1;
+const int M = 10;
+bool ran = false;
+
+int cells[M][M][M];
+int neighbourCells[M][M][M][27][3];
+int next[N];
+
+const double timeStepSize = 0.0001;
+
+inline double getDelta(int i, int j, int dim) {
+  
+  double dt = pos[i][dim] - pos[j][dim];
+  double absolute = fabs(dt);
+  
+  if (1 - absolute < absolute) {
+    if (dt > 0)
+      dt = 1 - absolute;
+    else
+      dt = -(1 - absolute);
+  }
+  
+  return dt;
+  
 }
+
+double boundedIncrease(double a, double b);
+
+inline const double calcDistance(double dx, double dy, double dz);
+
+inline void calculateForces(int i, int j, double *in);
+
+void setUp() {
+  pos[0][0] = 0.46;
+  pos[0][1] = 0.0;
+  pos[0][2] = 0.0;
+  
+  pos[1][0] = 0.53;
+  pos[1][1] = 0.0;
+  pos[1][2] = 0.0;
+  
+  
+  v[0][0] = 0.0;
+  v[0][1] = 0.0;
+  v[0][2] = 0.0;
+  
+  v[1][0] = 0.0;
+  v[1][1] = 0.0;
+  v[1][2] = 0.0;
+  
+  
+}
+
+float rand_FloatRange(float a, float b) {
+  return ((b - a) * ((float) rand() / RAND_MAX)) + a;
+}
+
+double randUnit() {
+  return rand_FloatRange(0.0, 1.0);
+}
+
+void randSetup() {
+  
+  for (int i = 0; i < N; i++) {
+    pos[i][0] = randUnit();
+    pos[i][1] = randUnit();
+    pos[i][2] = randUnit();
+    v[i][0] = 0;
+    v[i][1] = 0;
+    v[i][2] = 0;
+  }
+  
+}
+
+void printCSVFile(int counter) {
+  std::stringstream filename;
+  filename << "result-" << counter << ".csv";
+  std::ofstream out(filename.str().c_str());
+  
+  out << "x, y, z, force, dist, wrap, vel" << std::endl;
+  
+  for (int i = 0; i < N; i++) {
+    out << pos[i][0]
+    << ","
+    << pos[i][1]
+    << ","
+    << pos[i][2]
+    << ","
+    << pos[i][3]
+    << ","
+    << pos[i][4]
+    << ","
+    << pos[i][5]
+    << ","
+    << pos[i][6]
+    << std::endl;
+  }
+}
+
+double boundedIncrease(double cur, double change) {
+  
+  double d = cur + change;
+  
+  if (d > 1.0) {
+    
+    change = fabs(change);
+    double offset = change - ((long) change);
+    
+    double interim = offset + cur;
+    
+    if(interim > 1.0){
+      return 1.0 - interim;
+    } else{
+      
+      if(!(interim > 0)){
+        std::cout << "Easter egg!!!!" << interim << "," << change << "," << cur << std::endl;
+      }
+      return interim;
+    }
+  } else if (d < 0.0) {
+    change = fabs(change);
+    double offset = change - ((long) change);
+    
+    if(cur - offset > 0) {
+      return cur - offset;
+    }
+    else
+    {
+      return 1.0 - (offset - cur);
+    }
+    
+  }
+  
+  return d;
+}
+
+double force_potential(double r) {
+  
+  return 4 * a * ((12 * (std::pow(s, 12) / std::pow(r, 13))) - (6 * (std::pow(s, 6) / std::pow(r, 7))));
+  
+}
+
+const double calcDistance(double dx, double dy, double dz) {
+  const double distance = sqrt(
+                               dx * dx +
+                               dy * dy +
+                               dz * dz
+                               );
+  return distance;
+}
+
+void cell_update() {
+  
+  // all heads of all cells point to empty
+  for (int x = 0; x < M; x++) {
+    for (int y = 0; y < M; y++) {
+      for (int z = 0; z < M; z++) {
+        cells[x][y][z] = EMPTY;
+      }
+    }
+  }
+  
+  
+  for (int i = 0; i < N; i++) {
+    
+    // for each direction, calculate the index of the cell which should receive it
+    int x = pos[i][0] < 1 ? M - 1 : (int) (pos[i][0] / cellSize);
+    int y = pos[i][1] < 1 ? M - 1 : (int) (pos[i][1] / cellSize);
+    int z = pos[i][2] < 1 ? M - 1 : (int) (pos[i][2] / cellSize);
+    
+    // the current cell now points to the (soon to be previous) head of the cell
+    next[i] = cells[x][y][z];
+    
+    // the head of the cell is now this point
+    cells[x][y][z] = i;
+    
+    
+  }
+  
+  ran = true;
+  
+}
+
+void writeNeighbourCells() {
+  for (int x = 0; x < M; x++) {
+    for (int y = 0; y < M; y++) {
+      for (int z = 0; z < M; z++) {
+        
+        // generate neighbours in 3 dimensions
+        
+        int i = 0;
+        
+        for (int xx = x - 1; xx <= x + 1; xx++) {
+          for (int yy = y - 1; yy <= y + 1; yy++) {
+            for (int zz = z - 1; zz <= z + 1; zz++) {
+              if (xx >= 0 && xx < M && yy >= 0 && yy < M && zz >= 0 && zz < M) {
+                neighbourCells[x][y][z][i][0] = xx;
+                neighbourCells[x][y][z][i][1] = yy;
+                neighbourCells[x][y][z][i][2] = zz;
+              } else{
+                neighbourCells[x][y][z][i][0] = EMPTY;
+              }
+              
+              i++;
+              
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+void updateBody() {
+  
+  
+  double distA = 0;
+  
+  
+  for (int x = 0; x < M; x++) {
+    for (int y = 0; y < M; y++) {
+      for (int z = 0; z < M; z++) {
+        
+        
+        for (int di = 0; di < 27; di++) {
+          
+          int xx = neighbourCells[x][y][z][di][0];
+          if(xx == EMPTY) continue;
+          int yy = neighbourCells[x][y][z][di][1];
+          int zz = neighbourCells[x][y][z][di][2];
+          
+          
+          int i = cells[x][y][z];
+          // current cell
+          while (i != EMPTY) {
+            
+            int j = cells[xx][yy][zz];
+            double force[3];
+            bool dirty = false;
+            
+            force[0] = 0.0;
+            force[1] = 0.0;
+            force[2] = 0.0;
+            
+            
+            while (j != EMPTY) {
+              if (i != j) {
+                
+                double dx = getDelta(i, j, 0);
+                double dy = getDelta(i, j, 1);
+                double dz = getDelta(i, j, 2);
+                
+                double dSqrd = dx * dx + dy * dy + dz * dz;
+                
+                if (dSqrd <= rSqrd) {
+                  
+                  dirty = true;
+                  
+                  const double distance = sqrt(dSqrd);
+                  distA = distance;
+                  
+                  force[0] += dx * force_potential(distance);
+                  force[1] += dy * force_potential(distance);
+                  force[2] += dz * force_potential(distance);
+                }
+              }
+              
+              j = next[j];
+              
+            }
+            
+            assert(pos[i][0] < 1.0);
+            pos[i][0] = boundedIncrease(pos[i][0], timeStepSize * v[i][0]);
+            pos[i][1] = boundedIncrease(pos[i][1], timeStepSize * v[i][1]);
+            pos[i][2] = boundedIncrease(pos[i][2], timeStepSize * v[i][2]);
+            assert(pos[i][0] < 1.0);
+            
+            
+            
+            pos[i][3] = force[0];
+            pos[i][4] = distA;
+            pos[i][5] = xx;
+            pos[i][6] = v[i][0];
+            
+            if (dirty) {
+              
+              v[i][0] += timeStepSize * force[0];
+              v[i][1] += timeStepSize * force[1];
+              v[i][2] += timeStepSize * force[2];
+            }
+            
+            i = next[i];
+            
+          }
+          
+        }
+      }
+    }
+    
+  }
+  
+}
+
+
+int main() {
+  
+  srand(time(NULL));
+  
+  randSetup();
+  //  setUp();
+  printCSVFile(0);
+  writeNeighbourCells();
+  
+  const int timeSteps = 20000000;
+  const int plotEveryKthStep = 10000;
+  for (int i = 0; i < timeSteps; i++) {
+    cell_update();
+    updateBody();
+    if (i % plotEveryKthStep == 0) {
+      int i1 = i / plotEveryKthStep + 1;
+      std::cout << "Round " << i1 << std::endl;
+      if (i1 % 100 == 0) {
+        std::cout << "Round " << i1 << std::endl;
+      }
+      printCSVFile(i1); // Please switch off all IO if you do performance tests.
+    }
+  }
+  
+  return 0;
+}
+
+
--- default.cpp
+++ default.cpp
@@ -23,11 +23,12 @@
 #include <cmath>
 #include <iostream>
 #include <cassert>
+#include <vector>
 
-
-const int N = 2;
+const int N = 100;
 double pos[N][7];
 
+#define MOD(a, b) ((((a)%(b))+(b))%(b))
 #define EMPTY -1
 
 double v[N][3];
@@ -37,44 +38,81 @@ const double a = 0.01;
 const double s = a;
 const double cutoff = 10 * s;
 const double rSqrd = cutoff * cutoff;
-const double cellSize = 0.2;
-const int M = 25;
+
+
+const double skinDiameter = cutoff + 0.5 * cutoff;
+const double cellSize = skinDiameter;
+const int M = 7;
+
+double maximumDisplacementPerParticle[N][3];
+double maximumDistanceDisplaced[2];
+
+std::vector<int> *vertletList;
 
 int cells[M][M][M];
-int *neighbourCells[M][M][M];
+int neighbourCells[M][M][M][27][3];
 int next[N];
 
-const double timeStepSize = 0.0003;
+const double timeStepSize = 0.0001;
 
 inline double getDelta(int i, int j, int dim) {
 
   double dt = pos[i][dim] - pos[j][dim];
+  double absolute = fabs(dt);
 
-  if (1 - fabs(dt) < fabs(dt)) {
-    if (dt > 0)
-      dt = 1 - fabs(dt);
+  if (1 - absolute < absolute) {
+    if (dt < 0)
+      dt = 1 - absolute;
     else
-      dt = -(1 - fabs(dt));
+      dt = -(1 - absolute);
   }
 
   return dt;
 
 }
 
-double boundedIncrease(double a, double b);
-
 inline const double calcDistance(double dx, double dy, double dz);
 
-inline void calculateForces(int i, int j, double *in);
+inline double force_potential(double r) {
+
+  return 4 * a * ((12 * (std::pow(s, 12) / std::pow(r, 13))) - (6 * (std::pow(s, 6) / std::pow(r, 7))));
+
+}
+
+const double calcDistance(double dx, double dy, double dz) {
+  const double distance = sqrt(
+      dx * dx +
+      dy * dy +
+      dz * dz
+  );
+  return distance;
+}
+
+float rand_FloatRange(float a, float b) {
+  return ((b - a) * ((float) rand() / RAND_MAX)) + a;
+}
 
 void setUp() {
-  pos[0][0] = 0.46;
-  pos[0][1] = 0.0;
-  pos[0][2] = 0.0;
+  pos[0][0] = 0.44;
+  pos[0][1] = 0.5;
+  pos[0][2] = 0.5;
 
-  pos[1][0] = 0.53;
-  pos[1][1] = 0.0;
-  pos[1][2] = 0.0;
+  pos[1][0] = 0.56;
+  pos[1][1] = 0.5;
+  pos[1][2] = 0.5;
+
+//  pos[2][0] = 0.5;
+//  pos[2][1] = 0.53;
+//  pos[2][2] = 0.5;
+//
+//  pos[3][0] = 0.5;
+//  pos[3][1] = 0.46;
+//  pos[3][2] = 0.5;
+//
+//  pos[4][0] = 0.0;
+//  pos[4][1] = 0.0;
+//  pos[4][2] = 0.0;
+
 
 
   v[0][0] = 0.0;
@@ -86,10 +124,6 @@ void setUp() {
   v[1][2] = 0.0;
 
 
-}
-
-float rand_FloatRange(float a, float b) {
-  return ((b - a) * ((float) rand() / RAND_MAX)) + a;
 }
 
 double randUnit() {
@@ -134,39 +168,9 @@ void printCSVFile(int counter) {
   }
 }
 
-double boundedIncrease(double cur, double change) {
-
-  double d = cur + change;
-
-  if (d > 1.0) {
-    double offset = d - ((long) d);
-    double distToAxis = 1.0 - cur;
-
-    return (offset - distToAxis);
-  } else if (d < 0.0) {
-    d = std::abs(d);
-    double offset = d - ((long) d);
-    double interim = offset - cur;
-
-    return 1.0 - interim;
-  }
-
-  return d;
-}
-
-double force_potential(double r) {
-
-  return 4 * a * ((12 * (std::pow(s, 12) / std::pow(r, 13))) - (6 * (std::pow(s, 6) / std::pow(r, 7))));
-
-}
-
-const double calcDistance(double dx, double dy, double dz) {
-  const double distance = sqrt(
-      dx * dx +
-      dy * dy +
-      dz * dz
-  );
-  return distance;
+double wrap(double pos) {
+  double wrapped = fmod(pos, 1.0);
+  return wrapped > 0.0 ? wrapped : wrapped + 1;
 }
 
 void cell_update() {
@@ -180,33 +184,28 @@ void cell_update() {
     }
   }
 
-
   for (int i = 0; i < N; i++) {
 
-    int indices[3];
-
     // for each direction, calculate the index of the cell which should receive it
-    for (int dir = 0; dir < 3; dir++) indices[dir] = (int) (pos[i][dir] / cellSize);
+    int x = (int) (pos[i][0] / cellSize);
+    int y = (int) (pos[i][1] / cellSize);
+    int z = (int) (pos[i][2] / cellSize);
 
     // the current cell now points to the (soon to be previous) head of the cell
-    next[i] = cells[indices[0]][indices[1]][indices[2]];
+    next[i] = cells[x][y][z];
 
     // the head of the cell is now this point
-    cells[indices[0]][indices[1]][indices[2]] = i;
+    cells[x][y][z] = i;
 
-    //std::cout << "Particle" << i << " is in cell " << indices[0] << std::endl;
   }
 
 }
 
 void writeNeighbourCells() {
-
-
   for (int x = 0; x < M; x++) {
     for (int y = 0; y < M; y++) {
       for (int z = 0; z < M; z++) {
 
-        int *neighbours = new int[27 * 3];
         // generate neighbours in 3 dimensions
 
         int i = 0;
@@ -216,103 +215,164 @@ void writeNeighbourCells() {
             for (int zz = z - 1; zz <= z + 1; zz++) {
 
 
-              if (xx > 0 && xx < M && yy > 0 && yy < M && zz > 0 && zz < M) {
-                // TODO: Memory leak
-                ((int (*)[3]) neighbours)[i][0] = xx;
-                ((int (*)[3]) neighbours)[i][1] = yy;
-                ((int (*)[3]) neighbours)[i][2] = zz;
-              } else{
-                ((int (*)[3]) neighbours)[i][0] = EMPTY;
-              }
+              neighbourCells[x][y][z][i][0] = MOD(xx, M);
+              neighbourCells[x][y][z][i][1] = MOD(yy, M);
+              neighbourCells[x][y][z][i][2] = MOD(zz, M);
 
               i++;
 
             }
           }
         }
-
-        neighbourCells[x][y][z] = neighbours;
       }
     }
   }
-
-  printf("%d", ((int (*)[3]) neighbourCells[5][5][5])[0][0]);
 }
 
 void updateBody() {
 
-
   double distA = 0;
 
+  for (int i = 0; i < N; i++) {
+    double force[3];
 
+    force[0] = 0.0;
+    force[1] = 0.0;
+    force[2] = 0.0;
+
+    // for every neighbour j in the vertlet list
+    for (auto &j : vertletList[i]) {
+
+      // calculate difference in x,y,z
+      double dx = getDelta(i, j, 0);
+      double dy = getDelta(i, j, 1);
+      double dz = getDelta(i, j, 2);
+
+      // calculate the distance squared
+      double dSqrd = dx * dx + dy * dy + dz * dz;
+
+      // compare the distance with the cutoff squared
+      if (dSqrd <= rSqrd) {
+
+        const double distance = sqrt(dSqrd);
+
+        distA = distance;
+
+        double potential = force_potential(distance);
+
+        force[0] += dx * potential;
+        force[1] += dy * potential;
+        force[2] += dz * potential;
+      }
+    }
+
+    // perform the actual movement of the particles while taking into
+    // account the wrap around force
+    double displacement[3];
+    displacement[0] = timeStepSize * v[i][0];
+    displacement[1] = timeStepSize * v[i][1];
+    displacement[2] = timeStepSize * v[i][2];
+
+    maximumDisplacementPerParticle[i][0] += displacement[0];
+    maximumDisplacementPerParticle[i][1] += displacement[1];
+    maximumDisplacementPerParticle[i][2] += displacement[2];
+
+    // we keep track of the two maximum displacement's between
+    // particles and update them accordingly
+    double absDist = maximumDisplacementPerParticle[i][0] * maximumDisplacementPerParticle[i][0] +
+                     maximumDisplacementPerParticle[i][1] * maximumDisplacementPerParticle[i][1] +
+                     maximumDisplacementPerParticle[i][2] * maximumDisplacementPerParticle[i][2];
+
+    if (absDist > maximumDistanceDisplaced[0]) {
+      maximumDistanceDisplaced[1] = maximumDistanceDisplaced[0];
+      maximumDistanceDisplaced[0] = absDist;
+    } else {
+      if (absDist > maximumDistanceDisplaced[1]) {
+        maximumDistanceDisplaced[1] = absDist;
+      }
+    }
+
+    pos[i][0] = wrap(pos[i][0] + displacement[0]);
+    pos[i][1] = wrap(pos[i][1] + displacement[1]);
+    pos[i][2] = wrap(pos[i][2] + displacement[2]);
+
+    assert(pos[i][0] > 0.0);
+
+
+    pos[i][3] = force[0];
+    pos[i][4] = distA;
+    pos[i][6] = v[i][0];
+
+
+    v[i][0] += timeStepSize * force[0];
+    v[i][1] += timeStepSize * force[1];
+    v[i][2] += timeStepSize * force[2];
+
+
+  }
+
+
+}
+
+void createVertletLists() {
+  vertletList = new std::vector<int>[N];
+  for (int i = 0; i < N; i++) {
+    vertletList[i] = std::vector<int>(N / 8, EMPTY);
+  }
+}
+
+void updateNeighbourList() {
+
+  for (int i = 0; i < N; i++) {
+
+    // we use a vector to store the neighbours of the current cell
+    // we don't know how many there are in advance so vector is
+    // needed for this.
+    std::vector<int> &neighbours = vertletList[i];
+
+    // clear the previous neighbours
+    neighbours.clear();
+
+  }
+
+  // we iterate through all the cells in three dimensions
   for (int x = 0; x < M; x++) {
     for (int y = 0; y < M; y++) {
       for (int z = 0; z < M; z++) {
 
+        // for each of the 27 neighbours of this cell
+        for (int neighbourIndex = 0; neighbourIndex < 27; neighbourIndex++) {
 
-        for (int di = 0; di < 27; di++) {
-          int (*neighbours)[3] = (int (*)[3]) neighbourCells[x][y][z];
-
-          int xx = neighbours[di][0];
-          if(xx == EMPTY) continue;
-
-          int yy = neighbours[di][1];
-          int zz = neighbours[di][2];
+          // fetch the neighbours x,y,z coordinates which
+          // are precalculated
+          int xx = neighbourCells[x][y][z][neighbourIndex][0];
+          int yy = neighbourCells[x][y][z][neighbourIndex][1];
+          int zz = neighbourCells[x][y][z][neighbourIndex][2];
 
 
+          // fetch the "head" particle of the current cell
           int i = cells[x][y][z];
-          // current cell
+
+
+          // while the "head" continues to point at a particle
           while (i != EMPTY) {
 
+            // get the neighbours of particle i
+            std::vector<int> &neighbours = vertletList[i];
+
+            // fetch the "head" of the neighbouring cell
             int j = cells[xx][yy][zz];
-            double force[3];
-            bool dirty = false;
 
-            force[0] = 0.0;
-            force[1] = 0.0;
-            force[2] = 0.0;
-
-
+            // while the neighbour cell contains more particles
             while (j != EMPTY) {
+
               if (i != j) {
-
-                double dx = getDelta(i, j, 0);
-                double dy = getDelta(i, j, 1);
-                double dz = getDelta(i, j, 2);
-
-                double dSqrd = dx * dx + dy * dy + dz * dz;
-
-                if (dSqrd <= rSqrd) {
-
-                  dirty = true;
-
-                  const double distance = sqrt(dSqrd);
-                  distA = distance;
-
-                  force[0] += dx * force_potential(distance);
-                  force[1] += dy * force_potential(distance);
-                  force[2] += dz * force_potential(distance);
-                }
+                // j is a neighbour of i
+                neighbours.push_back(j);
               }
 
               j = next[j];
 
-            }
-
-            if (dirty) {
-              pos[i][0] = boundedIncrease(pos[i][0], timeStepSize * v[i][0]);
-              pos[i][1] = boundedIncrease(pos[i][1], timeStepSize * v[i][1]);
-              pos[i][2] = boundedIncrease(pos[i][2], timeStepSize * v[i][2]);
-
-
-              pos[i][3] = force[0];
-              pos[i][4] = distA;
-              pos[i][5] = xx;
-              pos[i][6] = v[i][0];
-
-              v[i][0] += timeStepSize * force[0];
-              v[i][1] += timeStepSize * force[1];
-              v[i][2] += timeStepSize * force[2];
             }
 
             i = next[i];
@@ -322,23 +382,49 @@ void updateBody() {
         }
       }
     }
-
   }
 
 }
 
+bool shouldUpdateNeighbourList() {
+  return maximumDistanceDisplaced[0] + maximumDistanceDisplaced[1] > skinDiameter - cutoff;
+}
 
 int main() {
 
-//  randSetup();
+  srand(time(NULL));
+
+//randSetup();
   setUp();
   printCSVFile(0);
+  createVertletLists();
   writeNeighbourCells();
+  cell_update();
+  updateNeighbourList();
+
+  for (int i = 0; i < N; i++) {
+    maximumDisplacementPerParticle[i][0] = 0.0;
+    maximumDisplacementPerParticle[i][1] = 0.0;
+    maximumDisplacementPerParticle[i][2] = 0.0;
+  }
+
+  int vertletSaved = 0;
 
   const int timeSteps = 20000000;
   const int plotEveryKthStep = 10000;
   for (int i = 0; i < timeSteps; i++) {
-    cell_update();
+    if (shouldUpdateNeighbourList()) {
+      cell_update();
+      updateNeighbourList();
+      for (int i = 0; i < N; i++) {
+        maximumDisplacementPerParticle[i][0] = 0.0;
+        maximumDisplacementPerParticle[i][1] = 0.0;
+        maximumDisplacementPerParticle[i][2] = 0.0;
+      }
+    } else {
+      vertletSaved++;
+    }
+
     updateBody();
     if (i % plotEveryKthStep == 0) {
       int i1 = i / plotEveryKthStep + 1;
@@ -349,6 +435,8 @@ int main() {
       printCSVFile(i1); // Please switch off all IO if you do performance tests.
     }
   }
+
+  std::cout << "Vertlet saved: " << vertletSaved << " timesteps" << std::endl;
 
   return 0;
 }
